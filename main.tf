4224358115ed46cc81691e754c250a407543dd55
DemoUser11221 <89487984+DemoUser11221@users.noreply.github.com>
2022-03-25 21:53:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   #access_key = var.access_key
   #secret_key = var.secret_key
-  #region = var.aws_region
+  region = var.aws_region
 }
 
 resource "aws_s3_bucket" "my-s3-bucket" {

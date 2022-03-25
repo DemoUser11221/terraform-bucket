027bada0bb2f30b89ee3471e84dfcd7ea5c64558
DemoUser11221 <89487984+DemoUser11221@users.noreply.github.com>
2022-03-25 21:19:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ provider "aws" {
 
 resource "aws_s3_bucket" "my-s3-bucket" {
   bucket_prefix = var.bucket_prefix
-  bucket = "my-s3-bucket"
+  bucket = "20220325154206460500000001"
   tags = var.tags
 }
